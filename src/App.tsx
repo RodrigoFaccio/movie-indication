767a2f427f60c3aa732a26ef960db1117e7c5b0a
murillo <murillodanton@hotmail.com>
2022-10-23 19:08:43 -0300

    stylish

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import axios from 'axios';
 import React,{useEffect, useState} from 'react';
-import {Container,Image,Title,Description,Button} from './styles'
+import {Container,Image,Title,Description,Button, MovieContainer, Genres, Forms, Input, Label, PreferenceContainer, MoviePreferences, TitlePreferences} from './styles'
 
 
 interface dataMovie{
     original_title: string;
     poster_path: string;
-    overview:string;
-
+    overview:string;    
+    genres:[
+      Object
+      :{
+      name:string
+    }]
+    popularity: number;
+    runtime: number;
 
 }
 function App() {
@@ -51,9 +57,13 @@ const [loading,setLoading] = useState(true)
 
     try {
       const id = (Math.random() * (10000 - 0) + 0);
-      const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${parseInt(id.toFixed())}?api_key=c21423006a220205b03494791e5e849e`);
-      setMovie(data)
-
+      const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${parseInt(id.toFixed())}?api_key=c21423006a220205b03494791e5e849e`); 
+      const popularity =  data.popularity.toFixed()   
+      if( popularity !== 30 ){
+        console.log(data.popularity)
+        return setMovie(data)
+      }      
+      
     } catch (error:any) {
       console.log(error.response.status)
 
@@ -78,16 +88,36 @@ const [loading,setLoading] = useState(true)
   
   return (
     <Container>
-      <Image src={'https://image.tmdb.org/t/p/original/'+movie?.poster_path}/>
-    
-      <Title>
-        {movie?.original_title}
-      </Title>
-      <Description>
-        {movie?.overview}
-      </Description>
+      <PreferenceContainer>
+        <TitlePreferences>
+          Escolha a sua preferência de filme
+        </TitlePreferences>
+        <Forms>
+          <Label>Gênero:</Label>
+          <Input><option value=''>{movie?.genres}</option></Input>
+          <Label>Ranking:</Label>
+          <Input><option value="">5</option></Input>
+          <Label>Duração em minutos:</Label>
+          <Input><option value="">{movie?.runtime}</option></Input>
+        </Forms>
+      </PreferenceContainer>
+      <MoviePreferences>
+        <MovieContainer>        
+          <Image src={'https://image.tmdb.org/t/p/original/'+movie?.poster_path}/>
+        
+            <Title>
+              {movie?.original_title}
+            </Title>                        
 
-      <Button onClick={()=>getMovie()}>Sortear</Button>
+        </MovieContainer>
+        <Title> Seu ranking é : {movie?.popularity.toFixed()}</Title>
+        <Description>
+          {movie?.overview}
+        </Description>
+
+        <Button onClick={()=>getMovie()}>Sortear</Button>
+      </MoviePreferences>
+      
     </Container>
   );
 }
--- src/styles.ts
+++ src/styles.ts
@@ -1,32 +1,125 @@
 import styled from 'styled-components';
 
+
 export const Container = styled.div`
   width: 100%;
-  min-height: 100%;
+  height: max-content;
   display: flex;
   justify-content: center;
   align-items: center;
   align-content: center;
-  background-color: gray;
-  flex-direction: column;
+  background-color: black;  
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;  
 `;
+
+export const MoviePreferences = styled.div`
+flex-direction: column;
+width: 50%;
+display: flex;
+justify-content: center;
+align-items: center;
+align-content: center;
+margin-top: 10px;
+` 
+
 export const Image = styled.img`
-    width:25%;
-    height: 50%;
+    width:80%;
+    height: 80%;
+    border-radius: 8px;
+    object-fit: fill;
 `;
 export const Title = styled.h3`
   color: black;
+  font-size: larger;
+  text-transform: uppercase;
+  color: white;
+  margin-bottom: -20px;
 `;
 export const Description = styled.p`
+  background-color: white;
   color: black;
-  width: 50%;
+  width: 82%;
+  border: 1px solid white;  
+  padding: 10px;
+  border-radius: 8px;
+  font-weight: 500;
 `;
 export const Button = styled.button`
   border: none;
-  background-color: aqua;
+  background-color: purple;
   margin-bottom: 40px;
   border-radius: 8px;
-  width: 100px;
-  height: 50px;
+  width: 120px;
+  height: 40px;
   cursor: pointer;
+  color: white;
+  font-weight: 600;
+  font-size: medium;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 `;
+
+export const MovieContainer = styled.div`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ align-content: center;
+ flex-direction: column;
+ border: 2px solid white;
+ border-radius: 8px;
+ height: 500px;
+ width: 500px;
+ background-color: none ;
+ margin: 0;
+ margin-top: 5px; 
+`
+
+export const Genres = styled.div`
+background-color: white;
+color: black;
+position: fixed;
+
+`
+export const Forms = styled.form`
+background-color: purple;
+margin: 10px;
+height: 90%;
+border: 1px solid purple;
+`
+export const Input = styled.select`
+color: purple;
+width: 100%;
+padding: 10px 2px;
+border: none;
+font-size: medium;
+font-weight: 700;
+
+
+:focus{
+  outline: none;
+}
+`
+
+export const Label = styled.label`
+font-weight: 700;
+color: white;
+margin: 10px 0;
+font-size: large;
+display: block;
+`
+export const PreferenceContainer = styled.div`
+flex-direction: column;
+width: 50%;
+height: 100%;
+justify-content: space-between;
+border: 2px solid white;
+border-radius: 8px;
+padding: 10px;
+margin: 30px;
+background-color: purple;
+`
+
+
+export const TitlePreferences = styled.h1`
+color: white;
+text-align: center;
+`
